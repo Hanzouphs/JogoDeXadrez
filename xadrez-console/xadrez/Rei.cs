add961caa761867d8578b9d5287787a7c1af48e4
Pedro Henrique <ppphsantos@gmail.com>
2022-12-27 05:50:17 -0300

    Restricoes de movimentos implementadas

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -2,7 +2,7 @@
 
 namespace xadrez
 {
-    internal class Rei : Peca
+     class Rei : Peca
     {
         public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) { 
 
@@ -15,7 +15,7 @@ namespace xadrez
 
         private bool podeMover(Posicao pos)
         {
-            Peca p =tab.peca(pos);
+            Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
         }
         public override bool[,] movimentosPossiveis()
